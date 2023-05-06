2533e0fc97d0c3e9b008e5d0f201cdc92f4e3b86
Wabona D <wdandi619@gmail.com>
2023-05-06 09:24:50 -0400

    added button

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
+#from flask_sqlalchemy import SQLAlchemy
+#i want to use python, genja and an api for this project
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +12,10 @@ def hello_world():
 @app.route('/tea')
 def tea():
     return "<h2> This is a tea page, made by us! .. <h2>"
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/button')
+def button():
+    return render_template("button.html")
